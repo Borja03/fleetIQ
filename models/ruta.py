d576acc4b49dd5efe234f5d01a05d906d6cc990a
2dam <2dam@TartangaLH.eus>
2024-11-28 09:36:12 +0100

    añadidas las que faltaban

--- models/ruta.py
+++ models/ruta.py
@@ -1,13 +1,20 @@
 from odoo import fields, models
 
+
 class Ruta(models.Model):
     _name = "FleetIQ.ruta"
     _description = "Rutas de los vehiculos"
 
+    # Campos básicos
     name = fields.Char(string="Nombre de la Ruta", required=True)
+    origen = fields.Char(string="Origen", required=True)
+    destino = fields.Char(string="Destino", required=True)
     distancia = fields.Float(string="Distancia (km)", required=True)
-    tiempo_estimado = fields.Float(string="Tiempo Estimado (horas)", required=True)
+    tiempo = fields.Float(string="Tiempo Estimado (horas)", required=True)
+    localizador = fields.Char(string="Localizador", required=True)
     fecha_creacion = fields.Date(string="Fecha de Creación", default=fields.Date.today)
+
+    # Campo de estado
     estado = fields.Selection(
         [
             ('nueva', 'Nueva'),
@@ -17,8 +24,10 @@ class Ruta(models.Model):
         string="Estado",
         default="nueva",
     )
+
     # Relación con RutaVehiculo
     ruta_vehiculo_ids = fields.One2many(
-        'FleetIQ.rutavehiculo', 'ruta_id', string="Vehículos Asignados"
+        'FleetIQ.rutaVehiculo', 'ruta_id', string="Vehículos Asignados"
     )
+
 
