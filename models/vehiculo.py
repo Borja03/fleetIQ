e54b4ef3b6cc6aa146d73bda94dc878c22d970a3
Alder <aldersanchez163@gmail.com>
2024-11-29 08:29:41 +0100

    modelos

---
+++ models/vehiculo.py
@@ -0,0 +1,12 @@
+from odoo import fields, models
+
+class Vehiculo(models.Model):
+    _name="fleetIQ.vehiculo"
+
+    id = fields.Integer(string="ID")
+    fecha_itv = fields.Datetime(string="Fecha de ITV")
+    fecha_matriculacion = fields.Datetime(string="Fecha de Matriculacion")
+    matricula = fields.Char(string="Número de Paquetes")
+    active = fields.Boolean(string="Activo")
+    capacidad = fields.Integer(string="Capacidad del vehiculo")
+    modelo = fields.Char(string="Modelo")
