76d9f0f96701cce2d96fe40bf9c94dbbc9e5a567
AdrianGarciaCaballero <144772061+AdrianGarciaCaballero@users.noreply.github.com>
2024-12-19 09:06:26 +0100

    Update vehiculo.py

--- models/vehiculo.py
+++ models/vehiculo.py
@@ -1,23 +1,28 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class Vehiculo(models.Model):
     _name = "fleetiq.vehiculo"
-    _description = "Vehículos"
 
     vehiculo_id = fields.Integer(string="ID")
     fecha_itv = fields.Datetime(string="Fecha de ITV")
     fecha_matriculacion = fields.Datetime(string="Fecha de Matriculación")
-    matricula = fields.Char(string="Matrícula", required=True)
-    active = fields.Boolean(string="Activo", default=True)
-    capacidad = fields.Integer(string="Capacidad del Vehículo")
+    matricula = fields.Char(string="Matrícula")
+    active = fields.Boolean(string="Activo")
+    capacidad = fields.Integer(string="Capacidad del vehiculo")
     modelo = fields.Char(string="Modelo")
 
     # Relación Many2one con el envío
     envio_id = fields.Many2one('fleetiq.envio', string="Envío")
 
-    # Relación One2many con RutaVehiculo
-    asignaciones_ruta = fields.One2many(
-        'fleetiq.rutavehiculo',  # Comodel
-        'vehiculo_id',          # Campo en RutaVehiculo que referencia Vehiculo
-        string="Asignaciones de Rutas"
-    )
+    # Relación Many2one con la ruta
+    ruta_id = fields.Many2one('fleetiq.ruta', string="Ruta")
+
+    @api.model
+    def _get_next_vehiculo_id(self):
+        """
+        Obtiene el siguiente ID de vehiculo incrementando el último registrado.
+        """
+        last_vehiculo = self.search([], order="vehiculo_id desc", limit=1)
+        if last_vehiculo:
+            return last_vehiculo.vehiculo_id + 1
+        return 1  # Si no hay envíos previos, empieza desde 1
