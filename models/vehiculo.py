e5787a3123ca9bfc95f0254e6ef855d3ff31a2f9
AdrianGarciaCaballero <144772061+AdrianGarciaCaballero@users.noreply.github.com>
2024-12-19 09:01:44 +0100

    Update vehiculo.py

--- models/vehiculo.py
+++ models/vehiculo.py
@@ -1,18 +1,23 @@
 from odoo import fields, models
 
 class Vehiculo(models.Model):
-    _name="fleetiq.vehiculo"
+    _name = "fleetiq.vehiculo"
+    _description = "Vehículos"
 
     vehiculo_id = fields.Integer(string="ID")
     fecha_itv = fields.Datetime(string="Fecha de ITV")
-    fecha_matriculacion = fields.Datetime(string="Fecha de Matriculacion")
-    matricula = fields.Char(string="Matrícula")
-    active = fields.Boolean(string="Activo")
-    capacidad = fields.Integer(string="Capacidad del vehiculo")
+    fecha_matriculacion = fields.Datetime(string="Fecha de Matriculación")
+    matricula = fields.Char(string="Matrícula", required=True)
+    active = fields.Boolean(string="Activo", default=True)
+    capacidad = fields.Integer(string="Capacidad del Vehículo")
     modelo = fields.Char(string="Modelo")
 
     # Relación Many2one con el envío
     envio_id = fields.Many2one('fleetiq.envio', string="Envío")
 
-    # Relación Many2one con la ruta
-    ruta_id = fields.Many2one('fleetiq.ruta', string="Ruta")
+    # Relación One2many con RutaVehiculo
+    asignaciones_ruta = fields.One2many(
+        'fleetiq.rutavehiculo',  # Comodel
+        'vehiculo_id',          # Campo en RutaVehiculo que referencia Vehiculo
+        string="Asignaciones de Rutas"
+    )
