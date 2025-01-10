aa260f309c49da9b0dd329577e0750256fbaffee
Borja03 <162575763+Borja03@users.noreply.github.com>
2025-01-10 08:17:00 +0100

    constraingts ruta

--- models/ruta.py
+++ models/ruta.py
@@ -1,10 +1,11 @@
 from odoo import fields, models, api
+from odoo.exceptions import ValidationError
 
 class Ruta(models.Model):
     _name = "fleetiq.ruta"
     _description = "Rutas de los vehículos"
 
-    localizador = fields.Char(string="Localizador", required=True, readonly=True)
+    localizador = fields.Char(string="Localizador", compute="_compute_localizador", store=True, readonly=True)
     name = fields.Char(string="Nombre de la Ruta", required=True)
     origen = fields.Char(string="Origen", required=True)
     destino = fields.Char(string="Destino", required=True)
@@ -33,4 +34,18 @@ class Ruta(models.Model):
         if self.origen and self.destino:
             self.localizador = (self.origen[:3] + self.destino[:3]).upper()
 
-    localizador = fields.Char(string="Localizador", compute="_compute_localizador", store=True)
+    @api.constrains('distancia', 'tiempo', 'origen', 'destino')
+    def _check_valid_data(self):
+        for record in self:
+            if record.distancia <= 0:
+                raise ValidationError("La distancia debe ser un valor positivo.")
+            if record.tiempo <= 0:
+                raise ValidationError("El tiempo estimado debe ser un valor positivo.")
+            if record.origen == record.destino:
+                raise ValidationError("El origen y el destino no pueden ser iguales.")
+
+    @api.model
+    def create(self, vals):
+        if 'origen' in vals and 'destino' in vals:
+            vals['localizador'] = (vals['origen'][:3] +'-'+ vals['destino'][:3]).upper()
+        return super(Ruta, self).create(vals)
